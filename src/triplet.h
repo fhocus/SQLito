fc893af4f504ec4870de36918f7c4843c6bf08fd
fhocus <fhocus.me@gmail.com>
2024-09-29 15:44:25 -0500

    add setters in triplet class

--- src/triplet.h
+++ src/triplet.h
@@ -16,6 +16,10 @@ public:
   O getSecond() const { return second_; }
   V getThird() const { return third_; }
 
+  void setFirst(T first) { first_ = first; }
+  void setSecond(O second) { second_ = second; }
+  void setThird(V third) { third_ = third; }
+
 private:
   T first_;
   O second_;
